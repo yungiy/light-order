5a3ea7b4c7c9f2ca234bf150648c97aadf75f660
yungiy <devyungiy@gmail.com>
2025-01-04 01:14:50 +0900

    [update] - 버튼 컴포넌트 고도화

--- src/components/order/classification.tsx
+++ src/components/order/classification.tsx
@@ -2,13 +2,13 @@ import Button from '../common/button';
 
 export default function Classification() {
   return (
-    <section className='flex row gap-1'>
-      <Button className='h-10 w-24 rounded-full font-light'>파스타</Button>
-      <Button className='h-10 w-24 rounded-full font-light'>피자</Button>
-      <Button className='h-10 w-24 rounded-full font-light'>사이드</Button>
-      <Button className='h-10 w-24 rounded-full font-light'>음료</Button>
-      <Button className='h-10 w-24 rounded-full font-light'>맥주</Button>
-      <Button className='h-10 w-24 rounded-full font-light'>와인</Button>
+    <section className='flex row gap-1 font-normal p-2'>
+      <Button className='h-10 w-24 rounded-full'>파스타</Button>
+      <Button className='h-10 w-24 rounded-full'>피자</Button>
+      <Button className='h-10 w-24 rounded-full'>사이드</Button>
+      <Button className='h-10 w-24 rounded-full'>음료</Button>
+      <Button className='h-10 w-24 rounded-full'>맥주</Button>
+      <Button className='h-10 w-24 rounded-full'>와인</Button>
     </section>
   );
 }
